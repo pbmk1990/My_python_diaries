4a921332e2f164894dc8d2cae5a7e58eea70925b
pbmk1990 <65609299+pbmk1990@users.noreply.github.com>
2020-05-23 06:55:07 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,13 @@
 print("Python adder")
-x=int(input("Enter first no:"))
-y=int(input("Enter second no:"))
-print("Sum is:",x+y)
+a='y'
+while a == 'y' or a == 'Y':
+    x = input("Enter first no:")
+    while x.isdigit() == False:
+        x=input("Enter a valid first no!:")
+    y = input("Enter second no:")
+    while y.isdigit() == False:
+        y=input("Enter a valid second no!:")
+    z=int(x)+int(y)
+    print("Sum is:",z)
+    a = input("Enter y to continue:")
+
